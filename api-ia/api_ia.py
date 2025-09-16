b74b086a22d4deefafa083a8fe62acd8a331c53d
alexdovale <62936602+alexdovale@users.noreply.github.com>
2025-09-16 11:20:54 -0300

    Update api_ia.py

--- api-ia/api_ia.py
+++ api-ia/api_ia.py
@@ -1,12 +1,12 @@
 import os
 import google.generativeai as genai
 from flask import Flask, request, jsonify
-from flask_cors import CORS # Necessário para permitir requisições do seu site
+from flask_cors import CORS
 
 # --- Configuração da API do Gemini ---
 # Pega a chave da variável de ambiente com o NOME "API_KEY"
-API_KEY = os.getenv("AIzaSyAWYcCfZ8P_vVlbLPSvpupsyLYT6GNF1Mg") 
-genai.configure(api_key=AIzaSyAWYcCfZ8P_vVlbLPSvpupsyLYT6GNF1Mg)
+API_KEY = os.getenv("API_KEY") 
+genai.configure(api_key=API_KEY)
 
 # Defina o modelo que você quer usar
 MODELO_IA = "gemini-1.5-pro-latest"
