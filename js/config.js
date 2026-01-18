f4cf8e5a51843a6f36588f23f184c29a2501f1ee
alexdovale <62936602+alexdovale@users.noreply.github.com>
2026-01-18 13:29:42 -0300

    Initialize Firebase and EmailJS configurations

--- js/config.js
+++ js/config.js
@@ -1,1 +1,21 @@
-config.js: Inicialização do Firebase e EmailJS.
+// js/config.js-config.js: Inicialização do Firebase e EmailJS.
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCrLwXmkxgeVoB8TwRI7pplCVQETGK0zkE",
+    authDomain: "pauta-ce162.firebaseapp.com",
+    projectId: "pauta-ce162",
+    storageBucket: "pauta-ce162.appspot.com",
+    messagingSenderId: "87113750208",
+    appId: "1:87113750208:web:4abba0024f4d4af699bf25"
+};
+
+export const emailJsConfig = {
+    publicKey: "aGL-Q2UJcD2tDpUKq",
+    serviceId: "service_r1nxe6a",
+    templates: {
+        delegacao: "template_jslp9ny",
+        anotacoes: "template_ynrwaxt"
+    }
+};
+
+export const APP_VERSION = "2.1 (Modular)";
